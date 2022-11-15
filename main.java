caab01df844023cf0b2ddbe98f1c78395cc990f9
genndy3 <113731406+genndy3@users.noreply.github.com>
2022-11-14 19:40:38 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int matriz[][] = new int[6][6];
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                System.out.println(matriz[x][y]);
+
+            }
+        }
+    }
+}
