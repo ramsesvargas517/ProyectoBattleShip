2a02f596688195f5c516b7c724c7bb5dd4b685bb
Pablo922 <114039633+Pablo922@users.noreply.github.com>
2022-11-15 16:35:01 -0600

    Add files via upload

---
+++ ProyectoPrograBattleShip.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc202.proyectoprograbattleship;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Pablo
+ */
+public class ProyectoPrograBattleShip {
+
+    public static void main(String[] args) {
+      byte contrincante=0;
+      
+      contrincante = Byte.parseByte(JOptionPane.showInputDialog(
+              "Para continuar, seleccione su contrincante \n"
+              +"1 - Jugador Real \n"
+              +"2 - Cpu \n"
+      ));
+      switch (contrincante){
+          case 1:
+              JOptionPane.showMessageDialog(null, 
+                      "Ha elegido: Jugador Real");
+              break;
+          case 2:
+              JOptionPane.showMessageDialog(null,
+                      "Ha elegido: Cpu");
+              break;
+          default:
+              JOptionPane.showMessageDialog(null,
+                      "Opción Inválida");
+              
+           }
+      
+      }
+      
+      
+      
+      
+        
+    }
+
