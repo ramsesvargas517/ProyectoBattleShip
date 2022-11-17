358f223a5fcd988237579fcad05cb39b50ef5ba9
ramsesvargas517 <113648420+ramsesvargas517@users.noreply.github.com>
2022-11-16 21:02:38 -0600

    se juntaron todos los comits para el avance

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,6 +16,38 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+
+        String NombreJugador = JOptionPane.showInputDialog 
+        ( "Introduzca el nombre del jugador: " );
+        System.out.println ("Se registro el nombre " 
+                + NombreJugador + "correctamente" );
+
+       byte contrincante= 0;
+
+      contrincante = Byte.parseByte(JOptionPane.showInputDialog(
+              "Para continuar, seleccione su contrincante \n"
+              +"1 - Jugador Real \n"
+              +"2 - Cpu \n"
+             ));
+      switch (contrincante){
+          case 1:
+              JOptionPane.showMessageDialog(null, 
+                      "Ha elegido: Jugador Real");
+              break;
+          case 2:
+              JOptionPane.showMessageDialog(null,
+                      "Ha elegido: Cpu");
+              break;
+          default:
+              JOptionPane.showMessageDialog(null,
+                      "Opción Inválida");
+              
+           }
+        
+        
+        
         int matriz[][] = new int [6][6];
         //este for se utiliza para listar el contenido de la matriz
         //se puede utilizar mas adelante en el proyecto
